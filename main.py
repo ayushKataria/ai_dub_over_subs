e932d15c2e55f09b338ca37f9a73d95dff0dcf76
ayush.kataria <akat21121997@gmail.com>
2024-09-17 16:13:36 +0530

    added some logs

--- main.py
+++ main.py
@@ -8,23 +8,27 @@ import argparse
 import get_gender
 from merge_audio_files import merge
 
-async def main(file_name: str, target_language: str):
+async def main(file_name: str, target_language: str, whisper_device: str):
     print(file_name, target_language)
     # Generate subtitles
-    transcribe(file_name, target_language)
+    print("Generating transciption and translating to target language")
+    transcribe(file_name, target_language, device=whisper_device)
 
     with open("subtitles.json", "r") as file:
         subtitles = json.load(file)
 
     # Generate instrumental track
+    print("Removing existing vocals from video")
     remove_vocals(file_name)
 
     # Get the gender of the speaker
+    print("Predicting the gender of speaker for tts")
     gender = get_gender.predict(file_name.replace(".mp4", "_Instruments.wav"))
     # gender = "Female"
 
     voice_to_use = None
     # Generate tts files
+    print("Generating TTS files")
     for subtitle in subtitles:
         file_name, voice_to_use = await atts(subtitle[f'text_{target_language}'], target_language, gender, voice_to_use, subtitle['end'] - subtitle['start'])
         subtitle['file_name'] = file_name
@@ -32,6 +36,7 @@ async def main(file_name: str, target_language: str):
     with open("subtitles.json", "w") as file:
         json.dump(subtitles, file)
     # Combine tts files
+    print("Merging TTS audio files and video")
     merge(folder_name=".\tts_files")
     
 
@@ -42,6 +47,7 @@ if __name__ == "__main__":
                     description='It takes a path to a video file and dubs the audio to a given language')
     parser.add_argument("-f", "--file", required=True, help="Video file path")
     parser.add_argument("-l", "--language", required=True, help="Target language")
+    parser.add_argument("-d", "--device", required=False, default="cuda", help="Device for whisper model (all other models are loaded in CPU for now)")
     
     args = parser.parse_args()
-    asyncio.run(main(args.file, args.language))
+    asyncio.run(main(args.file, args.language, args.device))
